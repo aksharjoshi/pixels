06209e889ebf1a5784b5b3e72b767b084b78a1ea
aksharjoshi <Akshar@Akshars-MacBook-Pro.local>
2015-06-24 16:14:48 -0700

    Facebook signin modularized

---
+++ server/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
+  MONGO_URI: process.env.MONGO_URI || 'localhost',
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || '64f7a5b6aaa8f50a26ffaa9bf95a25e7',
+  FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || 'Foursquare Client Secret',
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'erLb0xSjpMRHkH9fStQ93A_t',
+  GITHUB_SECRET: process.env.GITHUB_SECRET || 'GitHub Client Secret',
+  LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'LinkedIn Client Secret',
+  WINDOWS_LIVE_SECRET: process.env.WINDOWS_LIVE_SECRET || 'Windows Live Secret',
+  TWITTER_KEY: process.env.TWITTER_KEY || 'UoEEaQzM8zvRmDuVgWiXg8t8t',
+  TWITTER_SECRET: process.env.TWITTER_SECRET || '1IR37QVf5wvXn7aEskQJhAs6eiEBVSM92Ac2c3snPok3D0G3nX',
+  TWITTER_CALLBACK: process.env.TWITTER_CALLBACK || 'http://localhost:3000/',
+  YAHOO_SECRET: process.env.YAHOO_SECRET || 'Yahoo Client Secret'
+};
